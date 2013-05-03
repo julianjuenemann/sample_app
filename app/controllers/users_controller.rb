ba739e8889d591de8b00dc060e9ec095a9bdf94e
Julian Jünemann <jjuenemann@gmail.com>
2013-05-03 12:43:12 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+    before_filter :signed_in_user, 
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
+
 	def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
 
-def index
+  def index
     @users = User.paginate(page: params[:page])
   end
 
@@ -32,7 +34,7 @@ def index
     end
   end
   
-def edit
+  def edit
   end
 
   def update
@@ -45,6 +47,19 @@ def edit
     end
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   
 
   private
@@ -60,6 +75,7 @@ def edit
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+    
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
